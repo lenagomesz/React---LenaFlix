f28da2c77221ffaf323c19742f0046c24d5947de
Helena Gomes <87838594+lenagomesz@users.noreply.github.com>
2024-09-30 19:59:50 -0300

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Filmes e Séries. Todos os direitos reservados.</p>
+      <p>&copy; 2024 LenaFlix. Todos os direitos reservados.</p>
       <div className="social-media">
         <a href="/">Facebook</a>
         <a href="/">Twitter</a>
